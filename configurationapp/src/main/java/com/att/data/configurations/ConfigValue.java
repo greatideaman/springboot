160049ea6761d007f4f22497e9706b9fe961c3bf
Mikhail <shopping@laryukhin.com>
2021-12-18 22:25:16 -0500

    Seems to be working fine.

--- configurationapp/src/main/java/com/att/data/configurations/ConfigValue.java
+++ configurationapp/src/main/java/com/att/data/configurations/ConfigValue.java
@@ -1,23 +1,33 @@
 package com.att.data.configurations;
 
 /**
- * Data Model
+ * Data Model, contains configuration name and configuration id values. Id is
+ * equal to -1 by default if not provided.
  */
 public class ConfigValue {
     private String configName;
-    private int configId;
+    private int configId = -1; // not a required field
 
     public ConfigValue(String name, int id) {
+        checkName(name);
         this.configId = id;
         this.configName = name;
     }
 
-    public ConfigValue() {
-
+    public ConfigValue(String name) {
+        checkName(name);
+        this.configName = name;
     }
 
     public void setConfigName(String name) {
+        checkName(name);
         this.configName = name;
+    }
+
+    private void checkName(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("null or empty cofig name is not allowed");
+        }
     }
 
     public String getConfigName() {
--- configurationapp/src/main/resources/static/js/app.js
+++ configurationapp/src/main/resources/static/js/app.js
@@ -4,21 +4,90 @@
     }
 
     App.prototype.getData = function() {
-        $.ajax('',{
-
+        var yearMo = $('#timePeriod').val();
+        $.ajax({
+            url: "/configuration/"+yearMo,
+            success: function(result){
+                $('#tbody').empty();
+                $.each(result, function(key, val) {
+                    $('#tbody').append(htmlRow(yearMo, val.configId, val.configName))
+                })
+            }
         }).then(function(data) {
 
         })
     };
 
+    App.prototype.saveConfiguration = function() {
+        var yearMonth = $('#timePeriod').val();
+        var configName = $('#configName').val();
+        $.ajax({
+           url: '/configuration/'+ yearMonth,
+                type: 'POST',
+           data: {configName: configName},
+           success: function(data) {
+               app.getData();
+               $('#configName').val('');
+                    alert('Configuration added');
+                },
+                error: function(data) {
+                    alert('Something went wrong');
+            }
+        });
+    };
+    
+    App.prototype.deleteRow = function(yearMonth, id) {
+        $.ajax({
+                url: '/configuration/'+ yearMonth + '/' + id,
+                type: 'DELETE',
+                success: function(data) {
+                    app.getData();
+                    alert('Configuration removed');
+                },
+                error: function(data) {
+                    alert('Something went wrong');
+                }
+        });
+    };
+    
+    App.prototype.deleteAll = function() {
+        var yearMonth = $('#timePeriod').val();
+        $.ajax({
+            url: '/configuration/'+ yearMonth,
+                type: 'DELETE',
+            success: function(data) {
+                app.getData();
+                alert('All Configuration Deleted');
+            },
+            error: function(data) {
+                alert('Something went wrong');
+            }
+        });
+    };
+
+    htmlRow = function(yearmo, configId, configName) {
+        return '<tr><td class="tdstyle">' + yearmo + '</td>' + '<td class="tdstyle">' + configId + '</td><td class="tdstyle">' 
+               + configName + '</td><td class="tdstyle" ><a alt="Delete row"" href="javascript:app.deleteRow(\''+ yearmo + '\', ' + configId + ');">Delete</a>' +'</td></tr>';
+    }
+    
     App.prototype.init = function() {
         $('#configTable').DataTable({
-            scrollY: 300,
+            scrollY: 400,
             paging: false,
             sorting: false,
             searching: false,
             info: false
         });
+        app.getData();
+        $('#timePeriod').change(function() {
+            app.getData();
+        });
+        $('#saveButton').click(function() {
+            app.saveConfiguration();
+        }); 
+        $('#deleteButton').click(function() {
+            app.deleteAll();
+        }); 
     };
 
     window.app = new App;
